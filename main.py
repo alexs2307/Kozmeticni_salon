bcb32847fba0c385f3f0c2e5fc63ee3e367a5448
alexs2307 <80421870+alexs2307@users.noreply.github.com>
2022-03-06 20:42:25 +0100

    started price link

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/cenik")
+def price():
+    return render_template("cenik.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
