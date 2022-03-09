fe310bfe73a5255e4c27d954f595e81dfd47d0dd
alexs2307 <80421870+alexs2307@users.noreply.github.com>
2022-03-09 13:50:01 +0100

    created contact form

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request
+from flask_mail import Mail,Message
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']= 465
+app.config['MAIL_USERNAME']='freelancigaa2021@gmail.com'
+app.config['MAIL_PASSWORD']='Morje123'
+app.config['MAIL_USE_TLS']= False
+app.config['MAIL_USE_SSL']= True
+
+mail=Mail(app)
 
 
 @app.route("/")
@@ -11,6 +20,18 @@ def index():
 @app.route("/cenik")
 def price():
     return render_template("cenik.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
