000086b62f0f3f21a5df6ed65b00d9d9ca517e18
alexs2307 <80421870+alexs2307@users.noreply.github.com>
2022-03-29 21:44:24 +0200

    updated

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask, render_template,request
-from flask_mail import Mail,Message
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT']= 465
-app.config['MAIL_USERNAME']='freelancigaa2021@gmail.com'
-app.config['MAIL_PASSWORD']='Morje123'
-app.config['MAIL_USE_TLS']= False
-app.config['MAIL_USE_SSL']= True
 
-mail=Mail(app)
 
 
 @app.route("/")
